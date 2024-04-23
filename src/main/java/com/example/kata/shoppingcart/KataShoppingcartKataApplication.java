c4c5a1fdbf8afea179307e62cfb995ae1b73b866
progden <ccdrumer@gmail.com>
2024-04-23 14:46:19 +0800

    添加了 Gradle 項目設定和初始應用程式檔案
    在存儲庫中已添加多個項目設定和應用程式檔案。這些檔案透過 Gradle 建立了項目的結構並為項目的根目錄設定。也包含了一個基礎的 Spring Boot 應用程式檔案和相關的測試檔案。

---
+++ src/main/java/com/example/kata/shoppingcart/KataShoppingcartKataApplication.java
@@ -0,0 +1,13 @@
+package com.example.kata.shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataShoppingcartKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataShoppingcartKataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kata/shoppingcart/KataShoppingcartKataApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.kata.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = org.mockito.quality.Strictness.LENIENT)
+class KataShoppingcartKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
